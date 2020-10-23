0ea3bdd8eaa0b9b7bb145bd4b4cab7115a80cbd8
SuryaMahindra <reddy.nallabathuni@mahindra.com>
2020-10-23 12:24:04 +0530

    library use inside the app

--- app/src/main/java/com/mfc/autofin/MainActivity.java
+++ app/src/main/java/com/mfc/autofin/MainActivity.java
@@ -2,7 +2,10 @@ package com.mfc.autofin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.mfc.autofin.framework.Activity.AutoFinDashBoardActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, AutoFinDashBoardActivity.class));
     }
 }
