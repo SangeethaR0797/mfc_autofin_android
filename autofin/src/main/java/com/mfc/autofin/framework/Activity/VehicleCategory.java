3723b4204fc5d41b118cbf690e9f6f1cc5a93234
SangeethaRamakrishnan <tsangeetha0797@gmail.com>
2020-10-26 17:18:40 +0530

    1. Search View added in DashboardActivity. 2. VehicleCategory UI done. 3. Vehicle number category UI done

---
+++ autofin/src/main/java/com/mfc/autofin/framework/Activity/VehicleCategory.java
@@ -0,0 +1,17 @@
+package com.mfc.autofin.framework.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mfc.autofin.framework.R;
+
+public class VehicleCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_category);
+        
+    }
+}
