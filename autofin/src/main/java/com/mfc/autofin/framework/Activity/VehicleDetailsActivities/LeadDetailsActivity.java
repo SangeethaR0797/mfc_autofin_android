90c21b18075134f70e29d65e78393fa59a110ce3
SangeethaRamakrishnan <T.SANGEETHA@mahindra.com>
2020-11-16 15:06:39 +0530

    ViewDetails UI done

---
+++ autofin/src/main/java/com/mfc/autofin/framework/Activity/VehicleDetailsActivities/LeadDetailsActivity.java
@@ -0,0 +1,17 @@
+package com.mfc.autofin.framework.Activity.VehicleDetailsActivities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mfc.autofin.framework.R;
+
+public class LeadDetailsActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lead_details);
+    }
+}
