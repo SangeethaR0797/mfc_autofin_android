95e3121456b16837db3ffcd8c39092e9f3e6416a
SangeethaRamakrishnan <T.SANGEETHA@mahindra.com>
2020-11-18 09:56:15 +0530

    Document Upload Activity updated

--- autofin/src/main/java/kyc/DocumentUploadActivity.java
+++ autofin/src/main/java/kyc/DocumentUploadActivity.java
@@ -13,7 +13,10 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,8 +34,11 @@ import utility.AutoFinConstants;
 
 public class DocumentUploadActivity extends AppCompatActivity implements View.OnClickListener, ImageUploadCompleted {
 
-    private LinearLayout llpancard;
+    private LinearLayout llPhotoIDProof, llPanCard, llAadharCard, llVoterIdCard, llPassport, llResidenceProof, llRentAgreement, llElectricityBill, llResAadharCard, llBankDocs, llBankStatement, llSalarySlip, llForm16, llITR;
+    private TextView tvSkipPhotoIdProof, tvPanCardURL, tvAadharCardURL, tvVoterIdURL, tvPassportURL, tvSkipResidenceProof, tvRentAgreementURL, tvElectricityBillURL, tvResAadharURL, tvBankStatementURL, tvSalarySlipURL, tvForm16URL, tvITRURL;
+    private CheckBox cbSkipBankDocs, cbUploadDocsAgreeTAndC;
     private CaptureImage captureImage;
+    private Button btnUpdateDoc;
 
     private File file;
     private Uri fileUri;
@@ -41,7 +47,7 @@ public class DocumentUploadActivity extends AppCompatActivity implements View.On
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.document_upload_activity);
+        setContentView(R.layout.activity_doc_upload);
         initViews();
         Callpermissions(DocumentUploadActivity.this);
 
@@ -49,25 +55,156 @@ public class DocumentUploadActivity extends AppCompatActivity implements View.On
 
     private void initViews() {
         captureImage = new CaptureImage();
-        llpancard = findViewById(R.id.llpancard);
-        llpancard.setOnClickListener(DocumentUploadActivity.this);
+        llPanCard = findViewById(R.id.llPanCard);
+        llPhotoIDProof = findViewById(R.id.llPhotoIDProof);
+        llAadharCard = findViewById(R.id.llAadharCard);
+        llVoterIdCard = findViewById(R.id.llVoterIdCard);
+        llPassport = findViewById(R.id.llPassport);
+        llResidenceProof = findViewById(R.id.llResidenceProof);
+        llRentAgreement = findViewById(R.id.llRentAgreement);
+        llElectricityBill = findViewById(R.id.llElectricityBill);
+        llResAadharCard = findViewById(R.id.llResAadharCard);
+        llBankDocs = findViewById(R.id.llBankDocs);
+        llBankStatement = findViewById(R.id.llBankStatement);
+        llSalarySlip = findViewById(R.id.llSalarySlip);
+        llForm16 = findViewById(R.id.llForm16);
+        llITR = findViewById(R.id.llITR);
+        tvSkipPhotoIdProof= findViewById(R.id.tvSkipPhotoIdProof);
+        tvPanCardURL= findViewById(R.id.tvPanCardURL);
+        tvAadharCardURL= findViewById(R.id.tvAadharCardURL);
+        tvVoterIdURL= findViewById(R.id.tvVoterIdURL);
+        tvPassportURL= findViewById(R.id.tvPassportURL);
+        tvSkipResidenceProof= findViewById(R.id.tvSkipResidenceProof);
+        tvRentAgreementURL= findViewById(R.id.tvRentAgreementURL);
+        tvElectricityBillURL= findViewById(R.id.tvElectricityBillURL);
+        tvResAadharURL= findViewById(R.id.tvResAadharURL);
+        tvBankStatementURL= findViewById(R.id.tvBankStatementURL);
+        tvSalarySlipURL= findViewById(R.id.tvSalarySlipURL);
+        tvForm16URL= findViewById(R.id.tvForm16URL);
+        tvITRURL= findViewById(R.id.tvITRURL);
+        cbSkipBankDocs= findViewById(R.id.cbSkipBankDocs);
+        cbUploadDocsAgreeTAndC= findViewById(R.id.cbUploadDocsAgreeTAndC);
+        btnUpdateDoc= findViewById(R.id.btnUpdateDoc);
+        llPanCard.setOnClickListener(DocumentUploadActivity.this);
+        llAadharCard.setOnClickListener(this);
+        llVoterIdCard.setOnClickListener(this);
+        llPassport.setOnClickListener(this);
+        llResidenceProof.setOnClickListener(this);
+        llRentAgreement.setOnClickListener(this);
+        llElectricityBill.setOnClickListener(this);
+        llResAadharCard.setOnClickListener(this);
+        llBankDocs.setOnClickListener(this);
+        llBankStatement.setOnClickListener(this);
+        llSalarySlip.setOnClickListener(this);
+        llForm16.setOnClickListener(this);
+        llITR.setOnClickListener(this);
+        cbSkipBankDocs.setOnClickListener(this);
+        cbUploadDocsAgreeTAndC.setOnClickListener(this);
+        btnUpdateDoc.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.llpancard) {
+        if (v.getId() == R.id.llPanCard) {
             if (checkPermissions(DocumentUploadActivity.this)) {
                 captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.PANCARD);
             } else {
                 Callpermissions(DocumentUploadActivity.this);
             }
         }
+        else if (v.getId() == R.id.llAadharCard) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.AADHARCARD);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llVoterIdCard) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.VOTERIDCARD);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llPassport) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.PASSPORT);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llRentAgreement) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.RENTAL_AGREEMENT);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llElectricityBill) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.ELECTRICITY_BILL);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llResAadharCard) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.RES_AADHAR_CARD);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llBankStatement) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.BANK_STATEMENT);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llSalarySlip) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.SALARY_SLIP);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llForm16) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.FORM_16);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if (v.getId() == R.id.llITR) {
+            if (checkPermissions(DocumentUploadActivity.this)) {
+                captureImage.chooseImage(DocumentUploadActivity.this, AutoFinConstants.ITR);
+            } else {
+                Callpermissions(DocumentUploadActivity.this);
+            }
+        }
+        else if(v.getId()==R.id.cbSkipBankDocs)
+        {
+            if(cbSkipBankDocs.isChecked())
+            {}
+        }
+        else if(v.getId()==R.id.cbUploadDocsAgreeTAndC)
+        {
+            if(cbUploadDocsAgreeTAndC.isChecked())
+            {}
+        }
+        else if(v.getId()==R.id.btnUpdateDoc)
+        {}
+
+
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-               int x = 0;
+        int x = 0;
         if (resultCode == RESULT_OK) {
             if (OptionGalleryCamera.getInstance().getWhichImage().equals("Gallery")) {
                 try {
@@ -92,7 +229,7 @@ public class DocumentUploadActivity extends AppCompatActivity implements View.On
                         e.printStackTrace();
                     }
 
-                    new ImageUploadTask(DocumentUploadActivity.this,file.getPath(), "123456", "PAN", requestCode, this).execute();
+                    new ImageUploadTask(DocumentUploadActivity.this, file.getPath(), "123456", "PAN", requestCode, this).execute();
 
 
                 } catch (Exception e) {
@@ -109,7 +246,7 @@ public class DocumentUploadActivity extends AppCompatActivity implements View.On
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
-                        new ImageUploadTask(DocumentUploadActivity.this,captureImage.file.getPath(), "123456", "PAN", requestCode, this).execute();
+                        new ImageUploadTask(DocumentUploadActivity.this, captureImage.file.getPath(), "123456", "PAN", requestCode, this).execute();
 
 
                     }
@@ -151,7 +288,14 @@ public class DocumentUploadActivity extends AppCompatActivity implements View.On
 
     @Override
     public void onImageUploadCompleted(String Imageurl, int statuscode) {
-         Log.e("Image received","Image received " +Imageurl);
+        Log.e("Image received", "Image received " + Imageurl);
+
+    }
+
+    private void setFileOnTextView(String fileName,TextView textView)
+    {
+        textView.setText(fileName);
+        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_file_attachment, 0, R.drawable.ic_refresh, 0);
     }
 }
 
--- autofin/src/main/java/utility/AutoFinConstants.java
+++ autofin/src/main/java/utility/AutoFinConstants.java
@@ -6,5 +6,14 @@ public static final String DEALER_ID  = "DEALERID";
 public static final String USER_TYPE  = "USERTYPE";
 public static final String APP_NAME  = "APPNAME";
 public static final int PANCARD = 101;
-
+    public static final int AADHARCARD = 102;
+    public static final int VOTERIDCARD = 103;
+    public static final int PASSPORT = 104;
+    public static final int RENTAL_AGREEMENT = 105;
+    public static final int ELECTRICITY_BILL = 106;
+    public static final int RES_AADHAR_CARD = 107;
+    public static final int BANK_STATEMENT =108 ;
+    public static final int SALARY_SLIP =109 ;
+    public static final int FORM_16 = 110;
+    public static final int ITR =111 ;
 }
