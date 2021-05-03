73431187df64fb65d72ca42214b43308b65ea92b
Avinash_mfcwl <tilekar.avinash@mahindra.com>
2021-05-03 15:31:21 +0530

    AddResidentDetailsRequest created

---
+++ autofin/src/main/java/v2/model/request/AddResidentDetailsRequest.kt
@@ -0,0 +1,25 @@
+package v2.model.request
+
+data class AddResidentDetailsRequest(
+        var Data: ResidentDetailsData? = null,
+        var UserId: String? = null,
+        var UserType: String? = null
+)
+
+data class ResidentDetailsData(
+        var CustomerId: Int? = null,
+        var personalDetails: ResidentDetailsDataPersonalDetails? = null,
+        var residentialDetails: ResidentDetailsDataResidentialDetails? = null
+)
+
+data class ResidentDetailsDataPersonalDetails(
+        var HaveExistingEMI: Boolean? = null,
+        var PANNumber: String? = null,
+        var TotalEMI: Int? = null
+)
+
+data class ResidentDetailsDataResidentialDetails(
+        var CustomerCity: String? = null,
+        var NoOfYearInResident: Int? = null,
+        var ResidenceType: String? = null
+)
