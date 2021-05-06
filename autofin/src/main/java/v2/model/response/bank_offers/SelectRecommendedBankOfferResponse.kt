d77ef924c8476daf616424842fd22c4bbdd3577f
Avinash_mfcwl <tilekar.avinash@mahindra.com>
2021-05-06 13:31:53 +0530

    SelectRecommendedBankOfferResponse created

---
+++ autofin/src/main/java/v2/model/response/bank_offers/SelectRecommendedBankOfferResponse.kt
@@ -0,0 +1,8 @@
+package v2.model.response.bank_offers
+
+data class SelectRecommendedBankOfferResponse(
+        var data: Any?,
+        var message: String?,
+        var status: Boolean?,
+        var statusCode: Any?
+)
