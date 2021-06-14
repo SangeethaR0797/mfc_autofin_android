05c3c38d5a5338c43cb0626f3419dbf74f18c4d9
Avinash_mfcwl <tilekar.avinash@mahindra.com>
2021-06-14 12:43:31 +0530

    Android DashboardFragment.kt created

--- autofin/src/main/java/v2/view/DashboardFragment.kt
+++ autofin/src/main/java/v2/view/DashboardFragment.kt
@@ -5,11 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 import com.mfc.autofin.framework.R
 import utility.CommonStrings
 import utility.Global
@@ -21,38 +21,70 @@ import v2.view.base.BaseFragment
 
 class DashboardFragment : BaseFragment(), View.OnClickListener {
 
-    lateinit var btnRegisteredList:Button
-    lateinit var btnNewVehicle:Button
+    lateinit var ivBack: ImageView
+    lateinit var ivNotification: ImageView
+    lateinit var etSearch: EditText
+    lateinit var llSearch: LinearLayout
+    lateinit var rvMenu: RecyclerView
+
+
     lateinit var kycDocumentViewModel: MasterViewModel
-     private val hardCodedCustomerId="1724"
-    private val hardCodedCaseId="0242210415000012"
+    private val hardCodedCustomerId = "1724"
+    private val hardCodedCaseId = "0242210415000012"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         kycDocumentViewModel = ViewModelProvider(this).get(MasterViewModel::class.java)
-        kycDocumentViewModel.getKYCDocumentLiveData().observe(requireActivity()) { mApiResponse: ApiResponse? ->
-            onGetKYCDocumentResponse(mApiResponse!!)
-        }
+        kycDocumentViewModel.getKYCDocumentLiveData()
+            .observe(requireActivity()) { mApiResponse: ApiResponse? ->
+                onGetKYCDocumentResponse(mApiResponse!!)
+            }
 
 
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        btnRegisteredList=view.findViewById(R.id.btnRegisteredList)
-        btnNewVehicle=view.findViewById(R.id.btnNewVehicle)
-        btnRegisteredList.setOnClickListener(this)
-        btnNewVehicle.setOnClickListener(this)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.v2_fragment_dashboard, container, false)
+
+        ivBack = view.findViewById(R.id.iv_back)
+        ivNotification = view.findViewById(R.id.iv_notification)
+        etSearch = view.findViewById(R.id.et_search)
+        etSearch.isEnabled = false
+        llSearch = view.findViewById(R.id.ll_search)
+        rvMenu = view.findViewById(R.id.rv_menu)
+
+        ivBack.setOnClickListener(this)
+        ivNotification.setOnClickListener(this)
+        etSearch.setOnClickListener(this)
+        llSearch.setOnClickListener(this)
         return view
     }
 
     override fun onClick(v: View?) {
         if (v != null) {
             when (v.id) {
+                R.id.iv_back -> {
+                    activity?.onBackPressed()
+                }
+                R.id.iv_notification -> {
+                    openNotificationFragment()
+                }
+
+                R.id.ll_search -> {
+                   openSearchFragment()
+                }
+
+                R.id.ll_search -> {
+                    openSearchFragment()
+                }
+
                 R.id.btnRegisteredList -> {
                     kycDocumentViewModel.getKYCDocumentResponse(Global.baseURL + CommonStrings.KYC_UPLOAD_URL_END_POINT + hardCodedCustomerId)
                 }
+
                 R.id.btnNewVehicle -> {
                     navigateFromDashBoard(R.id.vehicleSelectionFrag2)
                 }
@@ -60,6 +92,14 @@ class DashboardFragment : BaseFragment(), View.OnClickListener {
         }
     }
 
+    fun openSearchFragment()
+    {
+        showToast("Search")
+    }
+    fun openNotificationFragment()
+    {
+        showToast("Notification")
+    }
 
     private fun onGetKYCDocumentResponse(mApiResponse: ApiResponse) {
         when (mApiResponse.status) {
