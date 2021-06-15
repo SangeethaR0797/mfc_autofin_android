d1b4a5a18fda0a1229886dea4c48277d9619a6ca
Avinash_mfcwl <tilekar.avinash@mahindra.com>
2021-06-15 18:04:53 +0530

    Notice Board Data Set Via API

--- autofin/src/main/java/v2/view/NoticeBoardAndNotificationFragment.kt
+++ autofin/src/main/java/v2/view/NoticeBoardAndNotificationFragment.kt
@@ -1,28 +1,81 @@
 package v2.view
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.mfc.autofin.framework.R
+import utility.CommonStrings
+import utility.Global
+import v2.model.request.CommonRequest
+import v2.model.response.CommonResponse
+import v2.model.response.NoticeBoardDataResponse
+import v2.model.response.NoticeData
+import v2.model_view.DashboardViewModel
+import v2.model_view.NoticeBoardViewModel
+import v2.service.utility.ApiResponse
+import v2.view.adapter.NoticeRecyclerViewAdapter
 import v2.view.base.BaseFragment
+import v2.view.callBackInterface.itemClickCallBack
 
 
-class NoticeBoardAndNotificationFragment : BaseFragment() {
+class NoticeBoardAndNotificationFragment : BaseFragment(), View.OnClickListener {
 
     lateinit var tvTitle: TextView
+    lateinit var ivBack: ImageView
+    lateinit var rvData: RecyclerView
+    lateinit var llData: LinearLayout
+
     lateinit var screenType: String
+    lateinit var noticeBoardViewModel: NoticeBoardViewModel
+    lateinit var noticeRecyclerViewAdapter: NoticeRecyclerViewAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        noticeBoardViewModel = ViewModelProvider(this).get(NoticeBoardViewModel::class.java)
+
+        noticeBoardViewModel.getNoticeBoardDetailsLiveData()
+            .observe(requireActivity()) { mApiResponse: ApiResponse? ->
+                onNoticeBoardDetails(mApiResponse!!)
+            }
+        noticeBoardViewModel.getNoticeBoardActionLiveData()
+            .observe(requireActivity()) { mApiResponse: ApiResponse? ->
+                onNoticeBoardActionResponse(mApiResponse!!)
+            }
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         tvTitle = view.findViewById(R.id.tv_title)
+        ivBack = view.findViewById(R.id.iv_back)
+        rvData = view.findViewById(R.id.rv_data)
+        llData = view.findViewById(R.id.ll_data)
+        llData.visibility=View.GONE
+
         tvTitle.text = screenType
+
+        ivBack.setOnClickListener(this)
+
+        if (screenType.equals("Notice Board")) {
+            noticeBoardViewModel.getNoticeBoardDetails(
+                CommonRequest(
+                    0, CommonStrings.DEALER_ID,
+                    CommonStrings.USER_TYPE
+                ),
+                Global.customerAPI_BaseURL + CommonStrings.NOTICE_BOARD_DETAILS_END_POINT
+            )
+        } else if (screenType.equals("Notification")) {
+
+        }
     }
 
     override fun onCreateView(
@@ -37,7 +90,93 @@ class NoticeBoardAndNotificationFragment : BaseFragment() {
         return view
     }
 
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.iv_back -> {
+                    activity?.onBackPressed()
+                }
+            }
+        }
+    }
 
+    private fun setNoticeBoardData(list: ArrayList<NoticeData>?) {
+        val layoutManager = LinearLayoutManager(activity)
+        rvData.layoutManager = layoutManager
+
+        noticeRecyclerViewAdapter =
+            NoticeRecyclerViewAdapter(activity as Activity, list, object : itemClickCallBack {
+                override fun itemClick(item: Any?, position: Int) {
+                    var notice = item as NoticeData
+                    if (notice.isNew == true) {
+                        noticeBoardViewModel.noticeBoardAction(
+                            CommonRequest(
+                                notice.noticeBoardId, CommonStrings.DEALER_ID,
+                                CommonStrings.USER_TYPE
+                            ),
+                            Global.customerAPI_BaseURL + CommonStrings.NOTICE_BOARD_ACTION_END_POINT
+                        )
+                    }
+
+                }
+            })
+        rvData.adapter = noticeRecyclerViewAdapter
+
+    }
+
+
+    //region observer
+    private fun onNoticeBoardDetails(mApiResponse: ApiResponse) {
+        when (mApiResponse.status) {
+            ApiResponse.Status.LOADING -> {
+                llData.visibility=View.GONE
+                showProgressDialog(requireContext())
+            }
+            ApiResponse.Status.SUCCESS -> {
+                hideProgressDialog()
+                llData.visibility=View.VISIBLE
+
+                val noticeBoardDataResponse: NoticeBoardDataResponse =
+                    mApiResponse.data as NoticeBoardDataResponse
+                setNoticeBoardData(noticeBoardDataResponse.data!!.notices as ArrayList<NoticeData>?)
+
+            }
+            ApiResponse.Status.ERROR -> {
+                llData.visibility=View.GONE
+                hideProgressDialog()
+                Log.i("SoftOfferFragment", ": " + ApiResponse.Status.ERROR)
+
+            }
+            else -> {
+                Log.i("SoftOfferFragment", ": ")
+            }
+        }
+
+    }
+    private fun onNoticeBoardActionResponse(mApiResponse: ApiResponse) {
+        when (mApiResponse.status) {
+            ApiResponse.Status.LOADING -> {
+                //showProgressDialog(requireContext())
+            }
+            ApiResponse.Status.SUCCESS -> {
+                hideProgressDialog()
+
+                val commonResponse: CommonResponse = mApiResponse.data as CommonResponse
+
+            }
+            ApiResponse.Status.ERROR -> {
+                hideProgressDialog()
+                Log.i("SoftOfferFragment", ": " + ApiResponse.Status.ERROR)
+
+            }
+            else -> {
+                Log.i("SoftOfferFragment", ": ")
+            }
+        }
+
+    }
+
+//endregion observer
 }
 
 
