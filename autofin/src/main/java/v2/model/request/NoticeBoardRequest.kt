b915f6704773c538efa1d99248f58af7e56b3365
Avinash_mfcwl <tilekar.avinash@mahindra.com>
2021-06-15 15:49:51 +0530

    NoticeboardEndPoint , Request Response Created

---
+++ autofin/src/main/java/v2/model/request/NoticeBoardRequest.kt
@@ -0,0 +1,7 @@
+package v2.model.request
+
+data class NoticeBoardRequest(
+    var Data: Int?,
+    var UserId: String?,
+    var UserType: String?
+)
--- autofin/src/main/java/v2/model/response/NoticeBoardDataResponse.kt
+++ autofin/src/main/java/v2/model/response/NoticeBoardDataResponse.kt
@@ -1,4 +1,15 @@
 package v2.model.response
 
-class NoticeBoardDataResponse {
-}
+data class NoticeBoardDataResponse(
+    var `data`: Data?,
+    var message: Any?,
+    var status: Boolean?,
+    var statusCode: String?
+)
+
+data class Data(
+    var newCount: Int?,
+    var notices: List<NoticeData>?,
+    var totalCount: Int?
+)
+
