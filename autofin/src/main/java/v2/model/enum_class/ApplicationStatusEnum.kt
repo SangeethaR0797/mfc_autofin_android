4863290a2a40480346d090e8ad04b0606ba04244
Avinash_mfcwl <tilekar.avinash@mahindra.com>
2021-06-17 10:28:13 +0530

    Enum Created

---
+++ autofin/src/main/java/v2/model/enum_class/ApplicationStatusEnum.kt
@@ -0,0 +1,7 @@
+package v2.model.enum_class
+
+enum class ApplicationStatusEnum(val value: String) {
+    Registered("Registered"),
+    KYC_Done("KYC Done"),
+    Employment_Details_Submitted("Employment Details Submitted")
+}
---
+++ autofin/src/main/java/v2/model/enum_class/ScreenTypeEnum.kt
@@ -0,0 +1,7 @@
+package v2.model.enum_class
+
+enum class ScreenTypeEnum(val value: String) {
+    Search("Search"),
+    Notification("Notification"),
+    Notice_Board("Notice Board")
+}
