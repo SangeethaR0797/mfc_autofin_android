7b1bdb40c0932d7437ffaef8aaea48998f248a58
Avinash_mfcwl <tilekar.avinash@mahindra.com>
2021-07-07 10:35:22 +0530

    Dealer Selection Added

--- app/src/main/java/com/mfc/autofin/MainActivity.java
+++ app/src/main/java/com/mfc/autofin/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.mfc.autofin.framework.Activity.AutoFinDashBoardActivity;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mLaunchAutofin;
     private TextView tvBuildDate;
+    String[] dealerCode = {"Dealer >> 242", "Dealer >> 2857"};
+    private Spinner spDealer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +31,24 @@ public class MainActivity extends AppCompatActivity {
 
         mLaunchAutofin = findViewById(R.id.launchAutofin);
         tvBuildDate = findViewById(R.id.tvBuildDate);
+        spDealer = findViewById(R.id.spDealer);
         tvBuildDate.setText("Build on " + BuildConfig.BUILD_TIMESTAMP.toString());
+
+        // ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_spinner_item, dealerCode);
+        ArrayAdapter ad = new ArrayAdapter(this, R.layout.item, R.id.tvItem, dealerCode);
+        spDealer.setAdapter(ad);
+
         mLaunchAutofin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //Intent autodashboard = new Intent(MainActivity.this, DocumentUploadActivity.class);
                 // Intent autodashboard = new Intent(MainActivity.this, AutoFinDashBoardActivity.class);
                 // Intent autodashboard = new Intent(MainActivity.this, AdditionalFieldsActivity.class);
-
+                String[] value = spDealer.getSelectedItem().toString().split(">>");
                 Intent autodashboard = new Intent(MainActivity.this, HostActivity.class);
                 autodashboard.putExtra(AutoFinConstants.APP_NAME, "MFCBusiness");
-                autodashboard.putExtra(AutoFinConstants.DEALER_ID, "242");
+                // autodashboard.putExtra(AutoFinConstants.DEALER_ID, "242");
+                autodashboard.putExtra(AutoFinConstants.DEALER_ID, value[1].toString().trim());
                 autodashboard.putExtra(AutoFinConstants.USER_TYPE, "Dealer");
                 startActivity(autodashboard);
             }
