cf0c360f7f52985f56cc6c32632101eca1e311f2
SangeethaRamakrishnan <T.SANGEETHA@mahindra.com>
2021-07-22 16:00:01 +0530

    Activity have been finished before navigating to Successive fragments from StockAPIFragment and VehicleSelectionFrag

--- autofin/src/main/java/v2/view/VehicleSelectionFrag.kt
+++ autofin/src/main/java/v2/view/VehicleSelectionFrag.kt
@@ -178,6 +178,7 @@ public class VehicleSelectionFrag : BaseFragment(), View.OnClickListener {
             var addLeadRequest: AddLeadRequest? =
                 data?.getParcelableExtra(CommonStrings.VEHICLE_DATA)
             if (addLeadRequest != null) {
+                requireActivity().finish()
                 navigateToAddOrUpdateVehicleDetails(addLeadRequest)
             }
         }
